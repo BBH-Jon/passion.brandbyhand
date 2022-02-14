58185ab64d9f036ba09fa40e795d7d5e2eb782cf
jon <jon@brandbyhand.dk>
2022-02-14 14:26:07 +0100

    Ved ikke helt hvordan jeg slipper for denne

--- wp-config.php
+++ wp-config.php
@@ -1,4 +1,6 @@
 <?php
+
+define( 'WP_CACHE', false ); // Added by WP Rocket
 /**
  * The base configuration for WordPress
  *
@@ -13,31 +15,45 @@
  * * Database table prefix
  * * ABSPATH
  *
- * @link https://wordpress.org/support/article/editing-wp-config-php/
+ * @link https://codex.wordpress.org/Editing_wp-config.php
  *
  * @package WordPress
  */
 
+ /*===============================================
+ =          Cookie Login Error           =
+ ===============================================*/
+ define('COOKIE_DOMAIN', $_SERVER['HTTP_HOST'] );
+
+
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'local' );
+define( 'DB_NAME', "brandbyhand_dk_db4" );
+
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', "brandbyhan" );
+
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'root' );
+define( 'DB_PASSWORD', "7w9Qmzq%" );
+
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', "mysql14.123hotel.dk" );
+
 
 /** Database Charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8' );
+define( 'DB_CHARSET', 'utf8mb4' );
+
 
 /** The Database Collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
 
-/**
+$table_prefix = 'wp_';
+
+
+/**#@+
  * Authentication Unique Keys and Salts.
  *
  * Change these to different unique phrases!
@@ -46,32 +62,51 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         '5qRkhXpjJpKZHWL8NkdvKJb2CN68yVtaj+Uq0qtLMud1PRpZ3ay8gG5/B1bYDJLyYC9bxhERfvMwASsKTPic1Q==');
-define('SECURE_AUTH_KEY',  'QJAhTDWGARnIybp+MqoVrO1HRpI+/3rKrWRxsvno6SabSQ6njbG8W87sefWhNvnOuZV8MYQMy9r35fJZVdBdUw==');
-define('LOGGED_IN_KEY',    'Nfyrm9nLeSpbAB1eFc2PuVIgdyLzO5+JRlPLdHA7CI5TkDx0Yofa3bO0VafoGHcCAWsw8mYh9NXg9Bkw5TqrGw==');
-define('NONCE_KEY',        'SokyTyaWc+REl3L4pr9lJwfLgB6kupHDljR4cq7LR/peBsi1XHCxwyfKeimfPirJaCZFzvlIK5dscMhkG0WSIA==');
-define('AUTH_SALT',        'fH0tONJrG7nY9fm6ku9BUXDzfQ4YaROcUifge5CIcZ75ul+xM1fo7q84oJ+GwkdQMH7wFC2mOgZByrlXW32fAg==');
-define('SECURE_AUTH_SALT', 'k7fnLz+vsmXEF7Ay8G/2AyFZeUHwUIBEkSRl0jwrNGjI4l8FR0UpoXUULFAcBRNMjumN99XJhEuDztZ98DKzRA==');
-define('LOGGED_IN_SALT',   'v+KyKlNQMMyTYnoHiJQWZ5DYRw1Sn6kPWj8nxP2y0Lksav5ChTrZ2vuBjncEMN48CwBeuL3nt2imK/fsMWPupg==');
-define('NONCE_SALT',       'MElEYF3KxkDWeJJ4u2+KNjwctVHs7qNjIcCx/jO1PJz79qDRFzkjBV4NtvFuh/ikxiNQG8MgSsRcxD+2IxPWOg==');
+define( 'AUTH_KEY',         'IAdaDNZb$sjv)ODpB7s6U,;;|[~wurq>[|<XN3T{pvb}[Yy2l^MY!YB$:q!f4rp*' );
 
-/**
- * WordPress Database Table prefix.
- *
- * You can have multiple installations in one database if you give each
- * a unique prefix. Only numbers, letters, and underscores please!
- */
-$table_prefix = 'wp_';
+define( 'SECURE_AUTH_KEY',  'TT??_QP(@B0EedI339>1bG~m!0[VdcVh?P(=Q6X.7*x&${ZjiuU0I99??HL2i5>b' );
 
+define( 'LOGGED_IN_KEY',    '7^oLjo,92t+ROK%Fr.<[LsgGJFwQD,Dd*KW2e]uOj%-&zr+w|>`(ox}y?%;F1sR$' );
+
+define( 'NONCE_KEY',        'W5iJ&rGecCLyYf,<:=Pfj}QTC(Q7hKlJLgJ{KqB@DY<GB|j!u,jMXZ(3J)f}P&:e' );
+
+define( 'AUTH_SALT',        '`8jVB0v.-`3yoO(|U=G!P}8|G.XfNZC}m&#nTbz/ Q>u!ZSoG|3DhEzpfWLS`hBx' );
+
+define( 'SECURE_AUTH_SALT', ',Xslz!#+|#r4!nu3z.4?-|tNCwJU2qaG_d+ZjIz0)/6`}Kp[-Yje-xs3KU]{1l%u' );
+
+define( 'LOGGED_IN_SALT',   'kjBX)Nqt|@tSOCQ,(_ms6lnx_FI179fhPqVQ5>pf2|D6=wVglt:[KwFsH_{<s0#{' );
+
+define( 'NONCE_SALT',       '@R4j5T%F3]v21gRD/Hx(i*];Y<_0.%K/9OdTivQa[-(IXI>+,ZWpKfig/[3fjZmX' );
 
 
+/* Set environment */
+if('5.103.120.186' == $_SERVER['REMOTE_ADDR'] || '5.57.48.78' == $_SERVER['REMOTE_ADDR'] || '5.57.50.200' == $_SERVER['REMOTE_ADDR']) {
+	define('ENVIRONMENT', 'DEVELOPMENT');
+} else{
+	define('ENVIRONMENT', 'PRODUCTION');
+}
+
+// If dev environment
+if(ENVIRONMENT == 'DEVELOPMENT') {
+	define('WP_DEBUG', true); // Allow debug
+define( 'WP_CACHE', false ); // Added by WP Rocket
+	define('DONOTCACHEPAGE', true); // Disallow cache
+} else {
+	define('WP_DEBUG', true);
+}
+
+/*KINT debugging*/
+if(file_exists('kint.phar')){
+	include 'kint.phar';
+	Kint\Renderer\RichRenderer::$folder = true; //show kint on bottom instead of where its called
+}
 
 /* That's all, stop editing! Happy publishing. */
 
 /** Absolute path to the WordPress directory. */
-if ( ! defined( 'ABSPATH' ) ) {
+if( ! defined( 'ABSPATH' ) ) {
 	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
 }
 
 /** Sets up WordPress vars and included files. */
-require_once ABSPATH . 'wp-settings.php';
+require_once( ABSPATH . 'wp-settings.php' );
